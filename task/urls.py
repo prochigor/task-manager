3118f43f26f7db8c3b3f34f9e66cce8a63fe1ef5
Ihor Prochnickiy <2prochigor@gmail.com>
2023-11-22 15:18:01 +0200

    add url to task detail

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from task.views import TaskListView
+from task.views import (
+    TaskListView,
+    TaskDetailView,
+)
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="car-detail"),
 ]
 
 app_name = "task"
