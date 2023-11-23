5a90b421e44b459c898ab2fe94b97bb5715490a7
Ihor Prochnickiy <2prochigor@gmail.com>
2023-11-23 11:08:53 +0200

    rewrite form validation deadline

--- task/forms.py
+++ task/forms.py
@@ -17,12 +17,6 @@ class WorkerCreationForm(UserCreationForm):
         )
 
 
-def clean_license_number_form(data):
-    if data < datetime.datetime.now():
-        raise ValidationError("Deadline can't be less than now")
-    return data
-
-
 class TaskCreateForm(forms.ModelForm):
     class Meta:
         model = Task
@@ -30,15 +24,6 @@ class TaskCreateForm(forms.ModelForm):
 
     def clean_deadline(self):
         data = self.cleaned_data["deadline"]
-        return clean_license_number_form(data)
-
-# class CarForm(forms.ModelForm):
-#     drivers = forms.ModelMultipleChoiceField(
-#         queryset=get_user_model().objects.all(),
-#         widget=forms.CheckboxSelectMultiple,
-#         required=False
-#     )
-#
-#     class Meta:
-#         model = Car
-#         fields = "__all__"
+        if data < datetime.datetime.now():
+            raise ValidationError("Deadline can't be less than now")
+        return data
